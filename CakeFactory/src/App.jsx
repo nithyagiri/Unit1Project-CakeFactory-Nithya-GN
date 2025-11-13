d5eb34cc1dcc885f5dd5026102fc80e6ccdd8ef9
nithya <nithi.coolz@gmail.com>
2025-11-12 23:52:04 -0600

    CSS added for ContactPage, SocialMedia Icons added to the Footer

--- CakeFactory/src/App.jsx
+++ CakeFactory/src/App.jsx
@@ -1,16 +1,20 @@
-
+import {useState} from 'react';
 import './App.css';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import {mockCake} from './test-data/mockCake';
 import ShopPage from './components/ShopPage.jsx';
+import HomePage from './components/HomePage.jsx';
+import ContactPage from './components/ContactPage.jsx';
 
 function App() {
-  
+  const [currentPage, setCurrentPAge] = useState('contact');
 return(
   <div id="body-container">
     <Header />
-    <ShopPage cakes= {mockCake}/>
+    {currentPage=='home' && <HomePage />}
+    {currentPage=='shop' && <ShopPage cakes= {mockCake}/>}
+    {currentPage=='contact' && <ContactPage />}
     <Footer />
   </div>
 )
--- CakeFactory/src/components/HomePage.jsx
+++ CakeFactory/src/components/HomePage.jsx
@@ -1,3 +1,4 @@
+import HomeImage from '../images/Home.jpeg';
 const HomePage =() =>{
     return(
         <>
@@ -5,16 +6,16 @@ const HomePage =() =>{
             <div className="main-content">
                 <h1>Welcome</h1>
                 <p>
-                        At Cake Factory, every cake tells my story. I am a passionate baker who has dedicated years to perfecting the art of baking, combining flavors, textures, and designs to create cakes that delight both the eyes and the taste buds. My love for baking has led me to experiment with countless recipes and styles, and my creations have been recognized for their creativity and attention to detail.
+                            At Cake Factory, every cake tells my story. I am a passionate baker who has dedicated years to perfecting the art of baking, combining flavors, textures, and designs to create cakes that delight both the eyes and the taste buds. My love for baking has led me to experiment with countless recipes and styles, and my creations have been recognized for their creativity and attention to detail.
                 </p>
-                <p>     I make it easy for you to bring your cake vision to life. You can browse my selection of cakes and personalize them exactly as you like—choose the flavor, size, frosting, decorations, and even add a custom message. Placing an order is simple, and you can update delivery details, add a new order, or cancel an existing one anytime, giving you full control over your sweet creations.
+                <p>         I make it easy for you to bring your cake vision to life. You can browse my selection of cakes and personalize them exactly as you like—choose the flavor, size, frosting, decorations, and even add a custom message. Placing an order is simple, and you can update delivery details, add a new order, or cancel an existing one anytime, giving you full control over your sweet creations.
                 </p>
                 <p>
-                        With Cake Factory, I combine my passion, creativity, and dedication to deliver more than just a cake—I deliver a memorable experience. Each cake is crafted with care and attention to detail, ensuring that it is not only delicious but also a beautiful centerpiece for your special moments.Cake Factory isn’t just an app — it’s a celebration of flavor, creativity, and connection.Download today 
+                            With Cake Factory, I combine my passion, creativity, and dedication to deliver more than just a cake—I deliver a memorable experience. Each cake is crafted with care and attention to detail, ensuring that it is not only delicious but also a beautiful centerpiece for your special moments.Cake Factory isn’t just an app — it’s a celebration of flavor, creativity, and connection.Download today 
                     and turn your next occasion into something unforgettable.
                 </p>
             </div>
-            <img src="/images/Home.jpeg" width="100%" alt="Cake image welcome page" /> 
+            <img src= {HomeImage} width="100%" alt="Cake image welcome page" /> 
         </main>
         </>
     );
