b6007429252f5b76ae61c3ec06160a5772e66974
nithya <nithi.coolz@gmail.com>
2025-11-19 14:05:02 -0600

    css added for OrderPage and ChecoutPage

--- CakeFactory/src/components/pages/checkout/CheckoutPage.jsx
+++ CakeFactory/src/components/pages/checkout/CheckoutPage.jsx
@@ -1,4 +1,5 @@
 import mockCake from "../../../test-data/mockCake";
+import  "./checkout.css";
 
   const CheckoutPage = ({cart, setCart,setCurrentPage,setEditingItemId, setSelectedCake, setOrderTotal }) =>{
 
@@ -31,29 +32,38 @@ import mockCake from "../../../test-data/mockCake";
     };
     const getCakeTotal = (item) => {
            const extras = (item.sizePrice || 0) + (item.fillingPrice || 0);
-           return item.basePrice + extras;
+           return  (item.basePrice + extras) * item.quantity;
 };
     return(
-        <div >
-            <button onClick={() => setCurrentPage("shop")}>← Back to shop</button>
-            <div className ="checkout-items">
-                <h2> Your Order</h2>
+        <div className ="checkout-container">
+            <div className ="checkout-left">
+            <div className="checkout-header">
+            <span>Product Description</span>
+            <span>Quantity</span>
+            <span>Price</span>
+            </div>
+
                 {cart.length === 0 ? (<p>Your Cart is empty.</p>) : (
                     cart.map((item) =>(
-                        <div key ={item.id} >
-                            <img src ={"https://i.ibb.co/" + item.imageId} alt ={item.name}/> 
-                        <div>
-                            <h3>{item.name}</h3>
+                        <div className="checkout-card" key ={item.id} >
+                            <img src ={"https://i.ibb.co/" + item.imageId} className="checkout-img" alt ={item.name}/> 
+                        <div className="checkout-details">
+                            <h3><bold>{item.name}</bold></h3>
                             {item.size && <p><b>Size:</b> {item.size}</p>}
                             {item.flavour && <p><b>Flavour:</b> {item.flavour}</p>}
                             {item.filling && <p><b>Filling:</b> {item.filling}</p>}
                             {item.message && <p><b>Message:</b> {item.message}</p>}
-                            <p><b>Price:</b> ${getCakeTotal(item)}  </p>
-                            <button  onClick={() => {
-                                handleEdit(item)}}>Edit</button>
-                            <button onClick={() => {
-                                handleDelete(item.id)}}>Delete</button>
+                            <button className="edit-btn" onClick={() => { handleEdit(item)}}>Edit</button>
+                        </div> 
+                        <div className="checkout-qty">
+                            <p>{item.quantity}</p>   
                         </div>
+                        <div className="checkout-price">
+                            <p><b>Price:</b> ${getCakeTotal(item)}  </p>    
+                        </div> 
+                        <div className="checkout-delete">       
+                            <button onClick={() => { handleDelete(item.id)}}>Delete</button>
+                        </div> 
                         </div> 
                     ))
                 )}
@@ -61,13 +71,14 @@ import mockCake from "../../../test-data/mockCake";
             <div className="checkout-summary">
                 <h2> Order Summary</h2>
                 <p><b> Order Total:</b>${CalculateTotal()}</p>
-                <button onClick={() => 
-                  { const total = CalculateTotal(); 
-                    setOrderTotal(total);         
-                    setCurrentPage('payment')
-                   }}>
-                    Checkout
+                <button className="checkout-btn" 
+                        onClick={() =>{ 
+                          const total = CalculateTotal(); 
+                          setOrderTotal(total);         
+                          setCurrentPage('payment')}}
+                >Checkout 
                 </button> 
+                <button className="continue-btn" onClick={() => { setCurrentPage("shop")}}> Continue Shopping </button>
             </div>
         </div>    
     );
